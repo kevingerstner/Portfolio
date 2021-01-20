742be21dcdf57a231121a8608f1c263e5a578dd4
SirK <sirkevinicus@gmail.com>
2021-01-19 18:20:41 -0600

    Reorganized File Structure, minimized more file sizes

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+    $(".nav-container").load("./common/navbar.html");
+    $(".project-nav-container").load("./common/project-navbar.html");
+
     $(".tog").click(function(){
         $("i", this).toggle();
     });
